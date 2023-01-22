64cffbb05f6119d4e82bf320e073e11e6aae5745
Marcin Pankiewicz-Paczyński <marcinpaczynski@o2.pl>
2023-01-22 21:54:28 +0100

    Add Rules to JS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = `papier`;
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
