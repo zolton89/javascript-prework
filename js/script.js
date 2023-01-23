32206c6e5fd62e76d971307452142a096998efb1
Marcin Pankiewicz-Paczyński <marcinpaczynski@o2.pl>
2023-01-23 20:30:39 +0100

    Fix else_if add prompt

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+if(randomNumber == 2){
+    computerMove = 'papier';
+}
+
+if(randomNumber == 3){
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -10,10 +30,10 @@ if(playerInput == '1'){
 
 if(playerInput == '2'){
     playerMove = 'papier';
-  }
+}
 
-if(playerInput == '3'){
+  if(playerInput == '3'){
     playerMove = 'nożyce';
-  }
+}
 
 printMessage('Twój ruch to: ' + playerMove);
