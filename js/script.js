6056fd68db633f3b810f8cc122452e343dad13ea
Marcin Pankiewicz-Paczyński <marcinpaczynski@o2.pl>
2023-01-23 20:46:44 +0100

    Add game score

--- js/script.js
+++ js/script.js
@@ -32,8 +32,49 @@ if(playerInput == '2'){
     playerMove = 'papier';
 }
 
-  if(playerInput == '3'){
+if(playerInput == '3'){
     playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == '1' && playerMove == '1' ){
+    printMessage('Remis');
+}
+
+if(computerMove == '1' && playerMove == '2' ){
+    printMessage('Ty wygrywasz!');
+}
+
+if(computerMove == '1' && playerMove == '3' ){
+    printMessage('Ty przegrywasz!');
+}
+
+if(computerMove == '2' && playerMove == '1' ){
+    printMessage('Przegrywasz!');
+}
+
+if(computerMove == '2' && playerMove == '2' ){
+    printMessage('Remis!');
+}
+
+if(computerMove == '2' && playerMove == '3' ){
+    printMessage('Ty wygrywasz!');
+}
+
+if(computerMove == '3' && playerMove == '1' ){
+    printMessage('Ty wygrywasz!');
+}
+
+if(computerMove == '3' && playerMove == '2' ){
+    printMessage('Ty przegrywasz!');
+}
+
+if(computerMove == '3' && playerMove == '3' ){
+    printMessage('Remis!');
+}
+
+if(computerMove == '1,2,3' && playerMove == 'nieznany ruch' ){
+    printMessage('Wybierz poprawny numer1');
+}
+1
